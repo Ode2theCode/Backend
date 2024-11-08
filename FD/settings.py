5e59cdf7d15931b28f1a46377c23398ad4d3b4e4
Kian <kianyarii1383@gmail.com>
2024-11-08 18:51:18 +0330

    fix: corsheaders add

--- FD/settings.py
+++ FD/settings.py
@@ -23,9 +23,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'drf_spectacular',
     'authentication',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -34,6 +36,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 ROOT_URLCONF = 'FD.urls'
 
