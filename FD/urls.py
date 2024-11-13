05030bd4a715146c0b73ab27cf9c195fccc2d759
Mahziar <seyedmahziar59@gmail.com>
2024-11-13 17:39:35 +0330

    feat: group model added

--- FD/urls.py
+++ FD/urls.py
@@ -4,7 +4,10 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
     path('authentication/', include('authentication.urls')),
+    path('groups/', include('groups.urls')),
+    
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
