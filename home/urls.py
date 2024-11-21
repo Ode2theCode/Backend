aa6a38bafebec0f59cefa22e1174d74c410d4916
KianYari <164557175+KianYari@users.noreply.github.com>
2024-11-21 13:00:28 +0330

    fix: user retrieve and update modified

--- home/urls.py
+++ home/urls.py
@@ -5,11 +5,11 @@ urlpatterns = [
     path('', HomeView.as_view()),
     path('suggestions/', SuggestionsView.as_view()),
     
-    path('time-slot/create/', TimeSlotCreateView.as_view()),
-    path('time-slots/', TimeSlotListView.as_view()),
-    path('time-slots/<int:id>/delete/', TimeSlotDeleteView.as_view()),
+    path('user-time-slots/create/', TimeSlotCreateView.as_view()),
+    path('user-time-slots/', TimeSlotListView.as_view()),
+    path('user-time-slots/<int:id>/delete/', TimeSlotDeleteView.as_view()),
     
-    path('group-time-slot/create/', GroupTimeSlotCreateView.as_view()),
+    path('group-time-slots/create/', GroupTimeSlotCreateView.as_view()),
     path('group-time-slots/', GroupTimeSlotListView.as_view()),
     path('group-time-slots/<int:id>/delete/', GroupTimeSlotDeleteView.as_view()),
     
