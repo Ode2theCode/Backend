e6901178e6f7050fe88f21f4dfba552fec94e167
Kian <kianyarii1383@gmail.com>
2024-12-12 18:25:29 +0330

    test: accounts

--- FD/urls.py
+++ FD/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('authentication/', include('authentication.urls')),
     path('groups/', include('groups.urls')),
     path('scheduling/', include('home.urls')),
-    path('chat/', include('chat.urls')),
 
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
