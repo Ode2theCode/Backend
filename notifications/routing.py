849f65c3748d8f136c24825ea8ac6404509e9ffd
Kian <kianyarii1383@gmail.com>
2024-12-15 17:36:15 +0330

    fix: notification

--- notifications/routing.py
+++ notifications/routing.py
@@ -3,5 +3,6 @@ from django.urls import re_path
 from .consumers import NotificationConsumer
 
 notifications_websocket_urlpatterns = [
-    re_path(r"ws/notifications/", NotificationConsumer.as_asgi()),
+    re_path(r"ws/notifications/(?P<token>[-\w.]+)/$", NotificationConsumer.as_asgi()),
+
 ]
