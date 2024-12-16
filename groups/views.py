d38a8d8ea1acd980da4c6152b49c0b629a1696f7
Kian <kianyarii1383@gmail.com>
2024-12-16 04:39:34 +0330

    feat: group apis profiling

--- groups/views.py
+++ groups/views.py
@@ -5,6 +5,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from rest_framework.pagination import PageNumberPagination
 
+from silk.profiling.profiler import silk_profile
 
 from authentication.models import User
 from .serializers import *
@@ -18,6 +19,7 @@ class GroupCreateView(APIView):
     permission_classes = [IsAuthenticated]
     serializer_class = GroupCreateSerializer
     
+    @silk_profile(name='group-create')
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -32,6 +34,7 @@ class GroupCreateView(APIView):
 class GroupRetrieveView(APIView):
     serializer_class = GroupRetrieveSerializer
     
+    @silk_profile(name='group-retrieve')
     def get(self, request, *args, **kwargs):
         try:
             group = GroupService.retrieve_group(kwargs.get('title'))
@@ -46,6 +49,7 @@ class GroupUpdateView(APIView):
     permission_classes = [IsAuthenticated, IsGroupOwner]
     parser_classes = [MultiPartParser, FormParser, JSONParser]
     
+    @silk_profile(name='group-update')
     def patch(self, request, *args, **kwargs):
         try:
             GroupService.update_group(kwargs.get('title'), request.data)
@@ -57,16 +61,20 @@ class GroupUpdateView(APIView):
 
 class GroupDeleteView(APIView):  
     permission_classes = [IsAuthenticated, IsGroupOwner]  
+    
+    @silk_profile(name='group-delete')
     def delete(self, request, *args, **kwargs):
         try:
             GroupService.delete_group(kwargs.get('title'))
             return Response("group deleted successfully", status=status.HTTP_200_OK)
         except ValidationError as e:
             return Response(e.detail.get('detail'), status=e.detail.get('status'))
-        
+    
+
 class GroupJoinRequestView(APIView):
     permission_classes = [IsAuthenticated]
     
+    @silk_profile(name='group-join-request')
     def post(self, request, *args, **kwargs):
         try:
             type = GroupService.join_request(kwargs.get('title'), request.user)
@@ -81,6 +89,8 @@ class GroupJoinRequestView(APIView):
        
 class GroupCancelRequestView(APIView):
     permission_classes = [IsAuthenticated]
+    
+    @silk_profile(name='group-join-request-cancel')
     def post(self, request, *args, **kwargs):
         try:
             GroupService.cancel_join_request(kwargs.get('title'), request.user)
@@ -94,6 +104,7 @@ class GroupPendingRequestView(APIView):
     serializer_class = GroupPendingRequestSerializer
     pagination_class = PageNumberPagination
     
+    @silk_profile(name='group-pending-request')
     def get(self, request, *args, **kwargs):
         try:
             group_requests = GroupService.pending_requests(kwargs.get('title'))
@@ -109,6 +120,7 @@ class GroupAcceptRequestView(APIView):
     permission_classes = [IsAuthenticated, IsGroupOwner]
     serializer_class = GroupAcceptRequestSerializer
     
+    @silk_profile(name='group-accept-request')
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -117,12 +129,13 @@ class GroupAcceptRequestView(APIView):
             return Response("request accepted successfully", status=status.HTTP_200_OK)
         except ValidationError as e:
             return Response(e.detail.get('detail'), status=e.detail.get('status'))
-       
-          
+
+
 class GroupDeclineRequestView(APIView):
     permission_classes = [IsAuthenticated, IsGroupOwner]
     serializer_class = GroupDeclineRequestSerializer
     
+    @silk_profile(name='group-decline-request')
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -132,11 +145,11 @@ class GroupDeclineRequestView(APIView):
         except ValidationError as e:
             return Response(e.detail.get('detail'), status=e.detail.get('status'))
 
-        
 
 class GroupLeaveView(APIView):
     permission_classes = [IsAuthenticated, IsGroupMember]
     
+    @silk_profile(name='group-leave')
     def post(self, request, *args, **kwargs):
         try:
             GroupService.leave_group(kwargs.get('title'), request.user)
@@ -150,6 +163,8 @@ class GroupKickView(APIView):
     serializer_class = GroupKickSerializer
     permission_classes = [IsAuthenticated, IsGroupOwner]
     
+    
+    @silk_profile(name='group-kick-member')
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -166,6 +181,7 @@ class GroupMemberListView(APIView):
     pagination_class = PageNumberPagination
     
     
+    @silk_profile(name='group-member-list')
     def get(self, request, *args, **kwargs):
         try:
             members = GroupService.member_list(kwargs.get('title'))
