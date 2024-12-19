08fc50929ef94b0ddb23f92750ccd0bbd00feead
Kian <kianyarii1383@gmail.com>
2024-12-20 01:25:41 +0330

    feat: celery deployment setup

--- home/tasks.py
+++ home/tasks.py
@@ -9,41 +9,67 @@ from groups.models import Group
 from home.services import UserTimeSlotService, GroupTimeSlotService
 from home.models import GroupTimeSlot
 
+from django.db.models import Case, When, Value, FloatField, F, ExpressionWrapper, Sum
+from django.db.models.functions import Coalesce
+
+from home.models import UserSuggestions
+
 load_dotenv()
 
-redis_url = os.getenv('REDIS_URL')
-redis_client = redis.StrictRedis.from_url(redis_url)
 
 @shared_task
 def find_matching_groups(user):
-    loc = None
-    if user.neighborhood:
-        loc = user.neighborhood
-    user_time_slots = UserTimeSlotService.get_user_time_slots(user)
-    groups = Group.objects.all()
-    group_matches = []
-    for group in groups:
-        group_time_slots = GroupTimeSlotService.get_group_time_slots(group.title)
-        total_overlap = 0
+    VALID_LEVELS = ['A1', 'A2', 'B1', 'B2', 'C1', 'C2']
+    user_level = user.level.upper()
+    level_index = VALID_LEVELS.index(user_level)
+    levels_to_consider = [user_level]
+    if level_index > 0:
+        levels_to_consider.append(VALID_LEVELS[level_index - 1])
+    if level_index < len(VALID_LEVELS) - 1:
+        levels_to_consider.append(VALID_LEVELS[level_index + 1])
+    
+    groups = Group.objects.filter(level__in=levels_to_consider).order_by('?')[:100]
+    
+    groups = list(groups)
 
-        for user_slot in user_time_slots:
-            for group_slot in group_time_slots:
-                if user_slot.day_of_week == group_slot.day_of_week:
-                    overlap = min(user_slot.end_time, group_slot.end_time) - max(user_slot.start_time, group_slot.start_time)
-                    if overlap > 0:
-                        total_overlap += overlap
-        if group.neighborhood == loc:
-            total_overlap *= 1.3
-        
-        if user not in group.members.all():    
-            group_matches.append((group, total_overlap))
-            
-        if len(group_matches) == 25:
-            break
-            
-    group_matches.sort(key=lambda x: x[1], reverse=True)
-    group_ids = [group[0].id for group in group_matches]    
-    return group_ids
+    user_time_slots = UserTimeSlotService.get_user_time_slots(user)
+    
+    groups = Group.objects.filter(id__in=[group.id for group in groups]).annotate(
+        total_overlap=Coalesce(
+            Sum(
+                ExpressionWrapper(
+                    Case(
+                        *[
+                            When(
+                                time_slots__day_of_week=user_slot.day_of_week,
+                                then=F('time_slots__end_time') - F('time_slots__start_time')
+                            )
+                            for user_slot in user_time_slots
+                        ],
+                        output_field=FloatField()
+                    ),
+                    output_field=FloatField()
+                )
+            ),
+            Value(0.0),
+            output_field=FloatField()
+        )
+    )
+
+    if user.neighborhood:
+        groups = groups.annotate(
+            total_overlap=ExpressionWrapper(
+                F('total_overlap') * Case(
+                    When(neighborhood=user.neighborhood, then=1.3),
+                    default=1,
+                    output_field=FloatField()
+                ),
+                output_field=FloatField()
+            )
+        )
+    
+    best_fit_groups = groups.order_by('-total_overlap')[:25]
+    return best_fit_groups
 
 @shared_task
 def cache_matching_groups():
@@ -52,4 +78,5 @@ def cache_matching_groups():
     
     for user in users:
         matching_groups = find_matching_groups(user)
-        redis_client.set(f'user:{user.id}:matching_groups', matching_groups)
+        matching_group_ids = [group.id for group in matching_groups]
+        UserSuggestions.objects.update_or_create(user_id=user.id, defaults={'group_ids': ','.join(map(str, matching_group_ids))})
