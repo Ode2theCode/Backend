619db27e24cd6b7650675999fd45f6a9c46a0850
Kian <kianyarii1383@gmail.com>
2024-12-31 05:06:39 +0330

    fix: chat base

--- chat/consumers.py
+++ chat/consumers.py
@@ -20,18 +20,16 @@ from rest_framework.response import Response
 from django.db.models import F
 
 class ChatConsumer(WebsocketConsumer):
-    connected_users = set()
+    connected_users = {}
     
     def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        self.room_group_name = f"chat_{self.room_name}"
         
         try:
             self.chat = Chat.objects.get(group__title=self.room_name)
         except Chat.DoesNotExist:
             self.close()
             
-        
         token = self.scope['url_route']['kwargs']['token']
         user = self.get_user_from_token(token)
         
@@ -39,9 +37,8 @@ class ChatConsumer(WebsocketConsumer):
             self.close()
         
         self.scope['user'] = user
-        self.connected_users.add(user.id)
+        self.connected_users[user.id] = self.connected_users.get(user.id, 0) + 1
 
-        
         if user not in self.chat.group.members.all():
             self.accept()
             self.send(text_data=json.dumps({
@@ -50,19 +47,20 @@ class ChatConsumer(WebsocketConsumer):
             }))
             self.close()
         
+        # Only join the room group
         async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name, self.channel_name
+            self.room_name,
+            self.channel_name
         )
 
         self.accept()
         
-        messages =  self.get_chat_messages()
+        messages = self.get_chat_messages()
         self.send(text_data=json.dumps({
             'type': 'chat_history',
             'messages': messages
         }, cls=DjangoJSONEncoder))
 
-    
     def get_chat_messages(self):
         return list(self.chat.messages.all().order_by('-timestamp').values(
             'content', 'sender__username', 'timestamp'
@@ -81,11 +79,15 @@ class ChatConsumer(WebsocketConsumer):
             return None
 
     def disconnect(self, close_code):
-
+        # Only leave the room group
         async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name, self.channel_name
+            self.room_name,
+            self.channel_name
         )
-        self.connected_users.remove(self.scope["user"].id)
+        
+        self.connected_users[self.scope['user'].id] = self.connected_users.get(self.scope['user'].id, 1) - 1
+        if self.connected_users[self.scope['user'].id] <= 0:
+            del self.connected_users[self.scope['user'].id]
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
@@ -97,22 +99,22 @@ class ChatConsumer(WebsocketConsumer):
             content=message,
             sender=user
         )
-        group_members = Group.objects.get(title=self.room_name).members.exclude(id__in=self.connected_users)
-        for member in group_members:
-            if member.id not in self.connected_users:
-                NotificationConsumer.send_notification(member, f"New message in {self.room_name} from {user.username}")
-
         
+        # Broadcast to room
         async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
+            self.room_name,
             {
-                "type": "chat_message", 
-                "message": db_message.content, 
+                "type": "chat_message",
+                "message": db_message.content,
                 "username": db_message.sender.username,
                 "timestamp": db_message.timestamp.isoformat(),
             }
         )
         
+        # Send notifications to offline members
+        group_members = self.chat.group.members.exclude(id__in=self.connected_users.keys())
+        for member in group_members:
+            NotificationConsumer.send_notification(member, f"New message in {self.room_name} from {user.username}")
 
     def chat_message(self, event):
 
@@ -121,4 +123,3 @@ class ChatConsumer(WebsocketConsumer):
             'username': event['username'],
             'timestamp': event['timestamp']
         }))
-
